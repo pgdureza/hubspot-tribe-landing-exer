965d7d94afc65a879bc2f29e335fe664340ff531
Patrick Dureza <pgdureza@gmail.com>
2018-05-12 18:24:38 +0800

    added toggleHeaderMedia()

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Please take note of the following:
 2. No images are stored in this project, all images are from your live site. This means that internet connection is still required to fetch images from the cdn
 3. Google Font 'Rubik' import only supports 400 and 300 since mockup only uses those font-weight values
 4. Embeded Wistia video has wide screen aspect ratio so it will not be 100% the same with the mockups.
+5. Requirements stated that header should be able to accept images for the video container. To test this, I've added a global function that can be called via the developer console and this should toggle Video and Image for the header media. Simply enter > toggleHeaderMedia() on the dev console 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Image from './Image'
-import Video from './Video';
+import WistiaEmbed from './WistiaEmbed';
 
-export default () => {
-  return (
-    <header>
-      <div className="overlay"> 
-        <div className="inner-width">
-          <div className="header-row">
-            <div className="header-column inner-width-mobile">
-              <Image className="logo" 
-                src={'https://offers.tribegroup.co/hs-fs/hubfs/AALandingPages/Template%20Asset/TRIBE%20Logo%201000x309.png'} 
-                widths={[160, 320]}/>
-              <h1> H1 Headline </h1>
-              <h2> H2 Subline </h2>
-              <p> Paragraph Style // Lorem ipsum dolor sit amet, nec no brute nusquam civibus. Cibo essent qui id, in eius antiopam similique nam, at nostrum corrumpit vel. Lorem ipsum dolor sit amet, nec no brute nusquam civibus. Cibo essent qui id, in eius antiopam similique nam, at nostrum. </p>
-              <div className="sign-up-wrapper">
-              <a className="btn sign-up" href="https://brand.tribegroup.co/sign-up">
-                <span>Sign Up for Free</span>
-              </a>
+export default class componentName extends Component {
+
+  state = {
+    isVideoMedia: true
+  }
+
+  componentDidMount(){
+    // create global function for showcasing image instead of wistia video
+    window.toggleHeaderMedia = () => {
+      this.setState({
+        isVideoMedia: !this.state.isVideoMedia
+      })
+    }
+  }
+
+  render() {
+    return (
+      <header>
+        <div className="overlay"> 
+          <div className="inner-width">
+            <div className="header-row">
+              <div className="header-column inner-width-mobile">
+                <Image className="logo" 
+                  src={'https://offers.tribegroup.co/hs-fs/hubfs/AALandingPages/Template%20Asset/TRIBE%20Logo%201000x309.png'} 
+                  widths={[160, 320]}/>
+                <h1> H1 Headline </h1>
+                <h2> H2 Subline </h2>
+                <p> Paragraph Style // Lorem ipsum dolor sit amet, nec no brute nusquam civibus. Cibo essent qui id, in eius antiopam similique nam, at nostrum corrumpit vel. Lorem ipsum dolor sit amet, nec no brute nusquam civibus. Cibo essent qui id, in eius antiopam similique nam, at nostrum. </p>
+                <div className="sign-up-wrapper">
+                <a className="btn sign-up" href="https://brand.tribegroup.co/sign-up">
+                  <span>Sign Up for Free</span>
+                </a>
+                </div>
               </div>
-            </div>
-            <div className="header-column video-wrapper">
-              <Video />
+              <div className="header-column media-wrapper">
+                { this.state.isVideoMedia ? (
+                  <WistiaEmbed hashedId="c6me94lwri" playerColor="#f848c4" />
+                ) : (
+                  <Image 
+                    widths={[300, 600, 900]} 
+                    src={'https://www.tribegroup.co/hs-fs/hubfs/AAWebsiteAssets/BRANDS/Brand%20Manager%20Profile%20Picture.jpg'} 
+                  />
+                )}
+                
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
-  )
+      </header>
+    )
+  }
 }
